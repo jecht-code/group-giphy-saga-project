d1ef1d47829eb0b060e38f361d94eca1ea0b7b96
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2024-02-24 12:41:05 -0500

    favorites PUT route created

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
     res.send(result.rows);
   })
   .catch((error) => {
-    console.log('Error querying DB.', error)
+    console.log('Error querying DB.', error);
     res.sendStatus(500);
   });
 });
@@ -36,12 +36,26 @@ router.post('/', (req, res) => {
 // update a favorite's associated category
 router.put('/:id', (req, res) => {
   // req.body should contain a category_id to add to this favorite image
+  const queryText = `UPDATE "favorites" SET "category_id" = $1 WHERE "id" = $2;`;
+  const updateFavorite = req.data
+  pool
+  .query(queryText, [updateFavorite[0], updateFavorite[1]])
   res.sendStatus(200);
 });
 
 // delete a favorite
 router.delete('/:id', (req, res) => {
-  res.sendStatus(200);
+  const queryText = `DELETE FROM "favorites" WHERE "id" = $1;`;
+  
+  pool
+  .query(queryText, [req.params.id])
+  .then((result) => {
+    res.sendStatus(201);
+  })
+  .catch((error) => {
+    console.log('Error deleting entry.', error);
+    res.sendStatus(500);
+  });
 });
 
 module.exports = router;
